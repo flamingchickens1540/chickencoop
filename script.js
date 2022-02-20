112c4ae5e5256a0284b1841933f2311a6be82917
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-02-19 22:45:17 -0800

    fix: remove background offset

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ function addButton(locationName, text, pilot) {
 
 function initCanvas() {
 	loadImage("./background.png").then((image) => {
-		ctx.drawImage(image, 10, 10, 780, 800);
+		ctx.drawImage(image, 0, 0, 780, 800);
 		ctx.font = "15px Arial";
         let file;
         try {
